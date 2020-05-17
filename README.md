c3f325461e6bfc333a8e8e5941ae4d24421683b0
Brian Smith <briaguya@gmail.com>
2020-05-17 07:22:33 -0500

    get more buttons, change start stuff, readme, etc.

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 The goal of this will be to test input against nx-tas scripts
 
 ## What this currently does:
-When used in conjuntion with nx-TAS (specifically [this script](https://github.com/briaguya/nx-TAS/blob/master/sd_card/scripts/script1.txt)), it will wait for the input we have defined on the first line (ABXY all pressed at the same time) and then start counting frames and A presses. The current frame count and A press count are output to the [deko console](https://github.com/switchbrew/switch-examples/tree/master/graphics/deko3d/deko_console). It's currently hardcoded to stop after all 30 presses have happened.
+1. When the application is loaded, it waits for LSTICK and RSTICK to be clicked in at the same time
+2. Once that input is recieved, P1's controller is polled and the input formatted and output to the [deko console](https://github.com/switchbrew/switch-examples/tree/master/graphics/deko3d/deko_console)
+
+## Testing with [nx-TAS](https://github.com/hamhub7/nx-TAS)
+1. Copy script1.txt to the nx-TAS scripts folder on your sd card
+2. Attach an nx-TAS virtual controller as P1
+3. Open nxTASter
+4. Use nx-TAS to [run script1.txt](https://github.com/hamhub7/nx-TAS#scripts)
 
 TODO: video
 
--- source/main.c
+++ source/main.c
@@ -8,12 +8,51 @@ int main(int argc, char **argv)
     consoleInit(NULL);
 
     // clear screen and home cursor
-    printf( CONSOLE_ESC(2J) );
+    printf(CONSOLE_ESC(2J));
+    bool running = false;
+    // #define CONSOLE_RED     CONSOLE_ESC(31;1m)
+    int currentConsoleColor = 31;
+    int currentLinePosition = 0;
 
     // todo: disconnect controllers here if we can
 
+    //     // Color codes and attributes
+    // for(int i=0; i<8; i++)
+    // {
+    //     printf(%1$dm);
+    //     printf(    CONSOLE_ESC(%1$dm) /* Set color */
+    //             "Default "
+    //             CONSOLE_ESC(1m) "Bold "
+    //             CONSOLE_ESC(7m) "Reversed "
+
+    //             CONSOLE_ESC(0m) /* revert attributes*/
+    //             CONSOLE_ESC(%1$dm)
+
+    //             CONSOLE_ESC(2m) "Light "
+    //             CONSOLE_ESC(7m) "Reversed "
+
+    //             "\n"
+    //             CONSOLE_ESC(0m) /* revert attributes*/
+    //             , i + 30);
+    // }
+
+    //         #define CONSOLE_RESET   CONSOLE_ESC(0m)
+    // #define CONSOLE_BLACK   CONSOLE_ESC(30m)
+    // #define CONSOLE_RED     CONSOLE_ESC(31;1m)
+    // #define CONSOLE_GREEN   CONSOLE_ESC(32;1m)
+    // #define CONSOLE_YELLOW  CONSOLE_ESC(33;1m)
+    // #define CONSOLE_BLUE    CONSOLE_ESC(34;1m)
+    // #define CONSOLE_MAGENTA CONSOLE_ESC(35;1m)
+    // #define CONSOLE_CYAN    CONSOLE_ESC(36;1m)
+    // #define CONSOLE_WHITE
+
+    // 
+    printf(CONSOLE_WHITE CONSOLE_ESC(1m));
+    printf("nxTASter v0.0.1\n");
+    printf("Press LSTICK and RSTICK on P1 to start/stop reading inputs\n\n");
+
     // Main loop
-    while(appletMainLoop())
+    while (appletMainLoop())
     {
         //Scan all the inputs. This should be done once for each frame
         hidScanInput();
@@ -21,15 +60,145 @@ int main(int argc, char **argv)
         //hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
         u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
 
+        // Check for both stick clicks press to start/stop
+        if ((kDown & KEY_LSTICK) && (kDown & KEY_RSTICK)) {
+            if (running) {
+                printf(CONSOLE_WHITE CONSOLE_ESC(1m));
+                printf("\nStopped reading input from P1\n");
+                running = false;
+                currentLinePosition = 0;
 
-
-        if (kDown & KEY_A) {
-            printf(CONSOLE_ESC(7m) " ");
-        } else {
-            printf(CONSOLE_ESC(0m) " ");
+            } else {
+                running = true;
+                printf(CONSOLE_WHITE CONSOLE_ESC(1m));
+                printf("Started reading input from P1\n");
+            }
         }
 
-        if (kDown & KEY_PLUS) break; // break in order to return to hbmenu
+        if (running) {
+            // If we're at the last color loop back to red
+            if (currentConsoleColor == 37) {
+                currentConsoleColor = 31;
+            }
+
+            if (currentLinePosition == 5) {
+                printf("\n");
+                currentLinePosition = 0;
+            }
+            
+            printf(CONSOLE_ESC(%1$dm) CONSOLE_ESC(1m), currentConsoleColor);
+
+    // KEY_A            = BIT(0),       ///< A
+    // KEY_B            = BIT(1),       ///< B
+    // KEY_X            = BIT(2),       ///< X
+    // KEY_Y            = BIT(3),       ///< Y
+    // KEY_LSTICK       = BIT(4),       ///< Left Stick Button
+    // KEY_RSTICK       = BIT(5),       ///< Right Stick Button
+    // KEY_L            = BIT(6),       ///< L
+    // KEY_R            = BIT(7),       ///< R
+    // KEY_ZL           = BIT(8),       ///< ZL
+    // KEY_ZR           = BIT(9),       ///< ZR
+    // KEY_PLUS         = BIT(10),      ///< Plus
+    // KEY_MINUS        = BIT(11),      ///< Minus
+    // KEY_DLEFT        = BIT(12),      ///< D-Pad Left
+    // KEY_DUP          = BIT(13),      ///< D-Pad Up
+    // KEY_DRIGHT       = BIT(14),      ///< D-Pad Right
+    // KEY_DDOWN        = BIT(15), 
+
+            printf(" ");
+            // todo: eat this copypasta
+            if (kDown & KEY_A) {
+                printf("A");
+            } else {
+                printf(" ");
+            };
+
+            if (kDown & KEY_B) {
+                printf("B");
+            } else {
+                printf(" ");
+            };
+
+            if (kDown & KEY_X) {
+                printf("X");
+            } else {
+                printf(" ");
+            };
+
+            if (kDown & KEY_Y) {
+                printf("Y");
+            } else {
+                printf(" ");
+            };
+
+            if (kDown & KEY_L) {
+                printf("L");
+            } else {
+                printf(" ");
+            };
+
+            if (kDown & KEY_ZL) {
+                printf("\\");
+            } else {
+                printf(" ");
+            };
+
+            if (kDown & KEY_R) {
+                printf("R");
+            } else {
+                printf(" ");
+            };
+
+            if (kDown & KEY_ZR) {
+                printf("/");
+            } else {
+                printf(" ");
+            };
+
+            if (kDown & KEY_PLUS) {
+                printf("+");
+            } else {
+                printf(" ");
+            };
+
+            if (kDown & KEY_MINUS) {
+                printf("-");
+            } else {
+                printf(" ");
+            };
+
+            if (kDown & KEY_DLEFT) {
+                printf("<");
+            } else {
+                printf(" ");
+            };
+
+            if (kDown & KEY_DUP) {
+                printf("^");
+            } else {
+                printf(" ");
+            };
+
+            if (kDown & KEY_DRIGHT) {
+                printf(">");
+            } else {
+                printf(" ");
+            };
+
+            if (kDown & KEY_DDOWN) {
+                printf("v");
+            } else {
+                printf(" ");
+            };
+
+            currentConsoleColor++;
+            currentLinePosition++;
+        }
+
+        
+
+        if (!running && (kDown & KEY_PLUS))
+            break; // break in order to return to hbmenu
 
         consoleUpdate(NULL);
     }
